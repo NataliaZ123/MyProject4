7f5dfc9c94d7948ce58922375c23a9571876260f
NataliaZ123 <123448567+NataliaZ123@users.noreply.github.com>
2023-02-02 13:34:54 +0700

    +

--- Main.java
+++ Main.java
@@ -1,55 +1,60 @@
 import java.util.Scanner;
 
 public class Main{
+	
 	public static void main(String[] args){
 	    Scanner in = new Scanner(System.in);
         String input = in.nextLine();
         System.out.println(calc(input));
 	}
-	public static String calc(String input){
-		int a, b, c;
-		String [] strings = input.split(" ");
-		if(strings.length != 3){
-			return("throws Exception");
-		}
-		char t = '1';
-		for (char ch : input.toCharArray()) {
-			t = ch;
-		}
-		if(t == ' '){
-			return("throws Exception");
-		}
-		if((strings[0].compareTo("1") != 0) && (strings[0].compareTo("2") != 0) && (strings[0].compareTo("3") != 0) && (strings[0].compareTo("4") != 0) && (strings[0].compareTo("5") != 0) && (strings[0].compareTo("6") != 0) && (strings[0].compareTo("7") != 0) && (strings[0].compareTo("8") != 0) && (strings[0].compareTo("9") != 0) && (strings[0].compareTo("10") != 0)){	
-			return("throws Exception");
-		}
-		if((strings[2].compareTo("1") != 0) && (strings[2].compareTo("2") != 0) && (strings[2].compareTo("3") != 0) && (strings[2].compareTo("4") != 0) && (strings[2].compareTo("5") != 0) && (strings[2].compareTo("6") != 0) && (strings[2].compareTo("7") != 0) && (strings[2].compareTo("8") != 0) && (strings[2].compareTo("9") != 0) && (strings[2].compareTo("10") != 0)){	
-			return("throws Exception");
-		}
+	
+	public static String calc(String input) {
 		try{
-            a = Integer.parseInt(strings[0]);
-			b = Integer.parseInt(strings[2]);
-			if((a > 10) || (b > 10) ||(a < 1)||(b < 1)){
-				return("throws Exception");
+			int a, b, c;
+			String [] strings = input.split(" ");
+			if(strings.length != 3){
+				throw new MyException();
 			}
-			switch (strings[1]){
-				case "+":
-			    	c = a + b;
-			    	return String.valueOf(c);
-				case "-":
-				    c = a - b;
-					return String.valueOf(c);
-				case "*":
-					c = a * b;
-					return String.valueOf(c);
-				case "/":
-					c = a/b;
-					return String.valueOf(c);
-				default:
-					return("throws Exception");
+			char t = '1';
+			for (char ch : input.toCharArray()) {
+				t = ch;
+			}
+			if(t == ' '){
+				throw new MyException();
+			}
+			if((strings[0].compareTo("1") != 0) && (strings[0].compareTo("2") != 0) && (strings[0].compareTo("3") != 0) && (strings[0].compareTo("4") != 0) && (strings[0].compareTo("5") != 0) && (strings[0].compareTo("6") != 0) && (strings[0].compareTo("7") != 0) && (strings[0].compareTo("8") != 0) && (strings[0].compareTo("9") != 0) && (strings[0].compareTo("10") != 0)){	
+				throw new MyException();
+			}
+			if((strings[2].compareTo("1") != 0) && (strings[2].compareTo("2") != 0) && (strings[2].compareTo("3") != 0) && (strings[2].compareTo("4") != 0) && (strings[2].compareTo("5") != 0) && (strings[2].compareTo("6") != 0) && (strings[2].compareTo("7") != 0) && (strings[2].compareTo("8") != 0) && (strings[2].compareTo("9") != 0) && (strings[2].compareTo("10") != 0)){	
+				throw new MyException();
+			}
+			try{
+				a = Integer.parseInt(strings[0]);
+				b = Integer.parseInt(strings[2]);
+				switch (strings[1]){
+					case "+":
+						c = a + b;
+						return String.valueOf(c);
+					case "-":
+						c = a - b;
+						return String.valueOf(c);
+					case "*":
+						c = a * b;
+						return String.valueOf(c);
+					case "/":
+						c = a / b;
+						return String.valueOf(c);
+					default:
+						throw new MyException();
+				}
+			}
+			catch(NumberFormatException e){
+				System.out.println("throws Exception");
 			}
 		}
-		catch(NumberFormatException e){
-			return("throws Exception");
+		catch(MyException e){
+			System.out.println("throws Exception");
 		}
+		return "0";
 	}
 }
---
+++ MyException.java
@@ -0,0 +1,6 @@
+class MyException extends Exception{//inherited from exception
+	//if left empty already works(gets thrown)
+	//public MyException(String description){
+	//	super(description);
+	//}
+}
